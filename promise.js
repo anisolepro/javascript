29d9d04b9dd65a9e642d515904d4448116729628
Anisole <64343922+anisolepro@users.noreply.github.com>
2022-10-23 11:23:08 +0500

    Update promise.js

--- promise.js
+++ promise.js
@@ -3,7 +3,34 @@ let p = new Promise((resolve, reject) => {
 	setTimeout(() => {
 		console.log('promise done')
 		resolve('resolved func')
-	}, 2000)
+	}, 1000)
 })
 
 p.then((d) => { console.log(d) })
+
+
+// promise chaining 
+
+let p1 = new Promise((resolve,reject)=>{
+	setTimeout(()=>{
+		console.log("promise 1 done")
+		resolve('promise 1 resolve return')
+	},2000)
+})
+
+p1.then(data=>{
+	console.log(data)
+	return new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			console.log('promise 2 done')
+			resolve('promise 2 resolve return')
+		},2000)
+		
+	})
+}).then(data=>{
+	console.log(data)
+	return 'instant promise 3 return'
+})
+.then(data=>{
+	console.log(data)
+})
