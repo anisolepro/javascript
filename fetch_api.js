647873f1b4f0c3fd54fab5a6127e4226f3460839
Anisole <64343922+anisolepro@users.noreply.github.com>
2022-10-23 17:48:24 +0500

    Update fetch_api.js

--- fetch_api.js
+++ fetch_api.js
@@ -3,7 +3,27 @@
 fetch('https://jsonplaceholder.typicode.com/posts/1')
   .then(response => {
 	  console.log(response.status)    // to check status code like 200 or 403 or 404
-	  return response.text()
+	  return response.json()
   })
   .then(data => console.log(data))
 
+// POST
+
+const data = {
+	"name": "morpheus",
+	"job": "leader"
+}
+
+const options = {
+	method: 'POST',
+	body: JSON.stringify(data),
+	headers: {
+		'Content-Type': 'application/json'
+	}
+}
+fetch('https://reqres.in/api/users')
+	.then(response => {
+		console.log(response.status)
+		return response.json()
+	})
+	.then(response => console.log(response))
